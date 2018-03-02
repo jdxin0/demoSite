8e624b987df6fa9c119d87c155e906321f036780
颜虎 <997948809@163.com>
2018-03-02 17:22:28 +0800

    requireJS插件列表

--- 201802/20180227/20180227001/scripts/main.js
+++ 201802/20180227/20180227001/scripts/main.js
@@ -11,6 +11,7 @@ require.config({
         "test":"exclamation"
     }
 });
+//requireJS插件列表https://github.com/requirejs/requirejs/wiki/Plugins
 require(['domReady','JQ','Util','test','text!review.txt'],function(domReady,$,util,test,txt){
 	console.log(0);
 	domReady(function(){
