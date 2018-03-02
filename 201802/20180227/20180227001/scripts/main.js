c6067c4b77fe8e968b348a0de9b165e890ede794
颜虎 <997948809@163.com>
2018-03-02 17:15:33 +0800

    更新requirejs插件实例

--- 201802/20180227/20180227001/scripts/main.js
+++ 201802/20180227/20180227001/scripts/main.js
@@ -7,11 +7,18 @@ require.config({
 	baseUrl: "scripts/helper",
     paths: {
         "JQ": "jquery",
-        "Util": "util"
+        "Util": "util",
+        "test":"exclamation"
     }
 });
-require(['domReady!','JQ','Util'],function(dom,$,util){
-	console.log(dom);
+require(['domReady','JQ','Util','test','text!review.txt'],function(domReady,$,util,test,txt){
+	console.log(0);
+	domReady(function(){
+		console.log(1);
+	});
+	domReady(function(){
+		console.log(2);
+	})
 	console.log($.fn.jquery);
 	util.incrementCounter();
 	util.printCurrentCounter();
