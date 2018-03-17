9db687f6c6ae959163cbc46d0a3eba186122006c
颜虎 <997948809@163.com>
2018-03-17 21:09:41 +0800

    RequireJS测试toURl方法

---
+++ 201802/20180227/20180227001/js/app/urlTest.js
@@ -0,0 +1,6 @@
+define(function(require){
+	var url = require.toUrl("tools/lottery");
+	// var lottery = require("./js/tools/lottery.js");
+	// var lottery = require(url);
+	return url||"lottery module have load";
+});
