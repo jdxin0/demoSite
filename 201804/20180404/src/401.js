867d1ad4eb74eb07433e938de6166345b99832e4
luojianet <997948809@163.com>
2018-04-07 23:08:23 +0800

    import data from assets/data.json

--- 201804/20180404/src/401.js
+++ 201804/20180404/src/401.js
@@ -1,7 +1,13 @@
 import "../css/normalize.css";
 import "../css/style.css";
-import txt from "./poem.txt";
+import txt from "../assets/poem.txt";
+import data from "../assets/data.json";
 
 var ele = document.getElementById("appenBox");
 // var poem = document.createTextNode(txt);
 ele.innerHTML=txt;
+var dataArr = [];
+for(var x in data){
+	dataArr.push(x);
+}
+document.querySelector(".jsonDataDiv").innerHTML = dataArr.join(" ");
