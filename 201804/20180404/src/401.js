9e70a0298424d446fe92d385ce94cb9eb1ffcee4
颜虎 <997948809@163.com>
2018-04-08 23:03:24 +0800

    import引入样式是没有顺序的，修改为@import url形式

--- 201804/20180404/src/401.js
+++ 201804/20180404/src/401.js
@@ -1,6 +1,5 @@
+import "../css/first.css";
 import * as d3 from "d3";
-import "../css/normalize.css";
-import "../css/style.css";
 import txt from "../assets/poem.txt";
 import data from "../assets/data.json";
 import * as xml from "../assets/data.xml";
@@ -8,14 +7,14 @@ import * as xml from "../assets/data.xml";
 var ele = document.getElementById("appenBox");
 // var poem = document.createTextNode(txt);
 ele.innerHTML=txt;
-var dataArr = [];
+var dataArr = ["data.json:"];
 for(var x in data){
 	dataArr.push(x);
 }
 document.querySelector(".jsonDataDiv").innerHTML = dataArr.join(" ");
 
 // d3.select("body").style("background-color", "black");  // 直接选择 ->并设置属性
-d3.selectAll("p").style("color", function() { // 选择 -> 动态设置属性
+d3.selectAll("p,pre").style("color", function() { // 选择 -> 动态设置属性
   return "hsl(" + Math.random() * 360 + ",100%,50%)";
 });
 window.xml = xml;
