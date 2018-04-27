f31bb6f60c09cb3675051b96ed3cde2bd9d98013
luojianet <997948809@163.com>
2018-04-27 16:15:05 +0800

    页面添加按钮2

--- 201804/20180419/src/app.js
+++ 201804/20180419/src/app.js
@@ -1,3 +1,5 @@
+// require("es5-shim");
+// require("promise-polyfill");
 var btn = document.getElementById("btn");
 var btn2 = document.getElementById("btn2");
 btn.onclick = function(){
@@ -6,4 +8,10 @@ btn.onclick = function(){
 		dynamicModule.jsonp();
 		dynamicModule.time();
 	}, "dynamic");
+}
+btn2.onclick = function(){
+	import(/* webpackChunkName: "dynamic" */ "./dynamic.js").then(function(dynamicModule){
+		dynamicModule.jsonp();
+		dynamicModule.time();
+	});
 }
