5227ea68ba005b66a50c727dbb080b79519c21f3
luojianet <997948809@163.com>
2018-10-08 18:49:31 +0800

    eslint配置文件

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 5
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-sequences": [
+            "error"
+        ],
+        "no-console": [0],
+        "no-empty": ["off"]//禁用规则
+    }
+};
