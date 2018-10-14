2919c93937ebc0c16d0c27f8c2cd583be7923263
luojianet <997948809@163.com>
2018-10-15 02:02:27 +0800

    copy webpack/demo3

--- 201810/webpack-xx-demo/babel-7/src/main.js
+++ 201810/webpack-xx-demo/babel-7/src/main.js
@@ -1,13 +1,15 @@
 function timer(t) {
-	return new Promise((resolve, reject) => {
-		setTimeout(function() {
-			resolve()
-		}, t);
-	})
-};
-(async () => {
-	for (let i = 0; i < 10; i++) {
-		await timer(1000);
-		console.log(i);
-	}
-})();
+    return new Promise((resolve,reject)=>{
+        setTimeout(function() {
+            resolve()
+        }, t);
+    }
+    )
+}
+;(async()=>{
+    for (let i = 0; i < 10; i++) {
+        await timer(1000);
+        console.log(i);
+    }
+}
+)();
--- 201810/webpack-xx-demo/babel-7/webpack.config.js
+++ 201810/webpack-xx-demo/babel-7/webpack.config.js
@@ -1,21 +1,16 @@
 const path = require('path');
-module.exports = {
-	entry: "./src/main.js",
-	output: {
-		path: path.resolve('dist'),
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [{
-			test: /\.js$/,
-			use: {
-				loader: 'babel-loader',
-				options: {
-					presets: 'env',
-					plugins: ["transform-runtime"]
-				}
-			},
-			exclude: /node_modules/
-		}]
-	}
+module.exports={
+    devtool:"source-map",
+    entry:"./src/main.js",
+    output:{
+        path:path.resolve('dist'),
+        filename:'bundle.js'
+    },
+    module:{
+        rules:[{
+            test:/\.js$/,
+            use: 'babel-loader',
+            exclude:/node_modules/
+        }]
+    }
 }
