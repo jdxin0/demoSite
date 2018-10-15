54a49a8b13ab71e916455264d49ad2a40df3da1f
luojianet <997948809@163.com>
2018-10-15 16:16:02 +0800

    深层if逻辑

--- 201810/if/demo1.js
+++ 201810/if/demo1.js
@@ -1,5 +1,15 @@
 function handlePayBtnClick(){
 	if(!haslogin){
 		login();
+	}else{
+		if(isChild){
+			msgbox.show('子账号不能购买');
+		}else if(isStopVip){
+			msgbox.show('暂停会员不能购买');
+		}else if(!isSuperVip){
+			msgbox.show('不是超级会员，请先购买会员');
+		}else{
+			
+		}
 	}
 }
