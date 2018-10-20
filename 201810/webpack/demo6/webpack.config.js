c6a5b0f89a17963959979aa17b5f0c2d7da372cc
luojianet <997948809@163.com>
2018-10-20 23:55:07 +0800

    配置文件

--- 201810/webpack/demo6/webpack.config.js
+++ 201810/webpack/demo6/webpack.config.js
@@ -1,15 +1,30 @@
 var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve('dist'),
-        filename: 'bundle.js'
-    },
-    module:{
-        rules: []
-    },
-    plugins: [],
-    devServer:{
-        contentBase: path.resolve('dist')
-    }
+	devtool: 'source-map',
+	entry: './src/js/index.js',
+	output: {
+		path: path.resolve('dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: ['style-loader', 'css-loader']
+		}, {
+			test: /\.js$/,
+			use: 'babel-loader',
+			include: path.resolve('src'),
+			exclude: /(node_modules|bower_components)/
+		}]
+	},
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin({
+			sourceMap: true
+		})
+	],
+	devServer: {
+		contentBase: path.resolve('dist')
+	}
 };
