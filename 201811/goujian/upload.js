617d632795b93cf949e2ebd0f1ee7ec2699513dc
luojianet <997948809@163.com>
2018-11-03 18:31:06 +0800

    构建系统

--- 201811/goujian/upload.js
+++
@@ -1,8 +0,0 @@
-client.scp(config.projectPath, {
-    host: '10.10.45.12',
-    username: 'devweb',
-    password: 'I48OJ34Y',
-    path: '/usr/local/zeus/htdocs/act.vip.xunlei.com/' + config.clientFolder + '/vip/2018/' + config.projectFolder
-}, function () {
-    console.log(chalk[getColor()].bold(`已上传服务器:${new Date()}`));
-});
