a54997d6ae7a1a02c16074bb916eb4fd8db1c42f
luojianet <997948809@163.com>
2018-11-05 10:23:12 +0800

    优化demo

--- 201810/webpack-xx-demo/js-module/source/js/index.js
+++ 201810/webpack-xx-demo/js-module/source/js/index.js
@@ -1,5 +1,6 @@
 import { formatDate} from '@luojianet/utility';
 var dateStr = formatDate(new Date(),'yyyy-MM-dd hh:mm:ss');
 document.getElementById('timer').innerText = dateStr;
+document.getElementById('timer').style.color='blue';
 var a=1,b=2;
 console.log(`${a},${b}`);
