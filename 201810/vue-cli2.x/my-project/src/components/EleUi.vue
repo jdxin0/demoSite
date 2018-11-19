7adee754091d97dda31591ae4aa5c9ed7d9c5ae9
luojianet <997948809@163.com>
2018-11-19 23:00:12 +0800

    234

---
+++ 201810/vue-cli2.x/my-project/src/components/EleUi.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="form">
+        <el-radio v-model="radio" :label="true">备选项</el-radio>
+        <el-radio v-model="radio" :label="false">备选项</el-radio>
+    </div>
+</template>
+<script>
+export default {
+    name: 'HelloWorld',
+    data() {
+        return {
+            radio: false
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
--- 201810/vue-cli2.x/my-project/src/router/index.js
+++ 201810/vue-cli2.x/my-project/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import EleUi from '@/components/EleUi'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'EleUi',
+      component: EleUi
     }
   ]
 })
