adacc63d0760474262b28810e61fcbc01843650a
luojianet <997948809@163.com>
2018-11-21 15:59:03 +0800

    遍历文件

--- 201811/fs-module/demo2.js
+++ 201811/fs-module/demo2.js
@@ -3,30 +3,26 @@ var fs = require('fs');
 var path = require('path');
 const stripJsonComments = require('./demo4');
 var filePath = path.resolve(__dirname);
+var sftpConfig = {};
 function fileDisplay(filePath) {
-    fs.readdir(filePath, function (err, files) {
-        if (err) {
-            console.warn(err);
-        } else {
-            if (files.includes('sftp-config.json')) {
-                console.log(`${filePath}\\sftp-config.json`);
-                var content = fs.readFileSync(`${filePath}\\sftp-config.json`, 'utf-8');
-                var sftpData = stripJsonComments(content,{whitespace:false});
-                // var sftpConfig = {};
-                // ({
-                //     host:sftpConfig.host,
-                //     username:sftpConfig.username,
-                //     password:sftpConfig.password
-                // }=sftpData);
-                console.log(sftpData);
-            } else {
-                if (path.resolve(filePath + '/..') !== filePath) {
-                    var upFolder = path.resolve(filePath + '/..');
-                    fileDisplay(upFolder);
-                }
-            }
+    var files = fs.readdirSync(filePath);
+    if (files.includes('sftp-config.json')) {
+        console.log(`${filePath}\\sftp-config.json`);
+        var content = fs.readFileSync(`${filePath}\\sftp-config.json`, 'utf-8');
+        var sftpData = stripJsonComments(content,{whitespace:false});
+        sftpData = JSON.parse(sftpData);
+        ({
+            host:sftpConfig.host,
+            user:sftpConfig.user,
+            password:sftpConfig.password
+        }=sftpData);
+    } else {
+        if (path.resolve(filePath + '/..') !== filePath) {
+            var upFolder = path.resolve(filePath + '/..');
+            fileDisplay(upFolder);
         }
-    });
+    }
 }
 
 fileDisplay(filePath);
+console.log(sftpConfig);
